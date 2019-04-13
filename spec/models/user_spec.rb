90f4be382dca5e0a615ae44aafaacc047a04c649
Nick Lindeberg <lindebergnick@gmail.com>
2019-04-13 16:35:46 -0600

    Adds model test for favorites

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:email) }
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+  end
+  
+  describe 'relationships' do
+    it { should have_many(:favorites) }
+  end
 end
